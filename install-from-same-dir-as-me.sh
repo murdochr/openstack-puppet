c82d565ef1050ac75d95bc3d019f1310be165fa5
root <root@ub-compute-3.(none)>
2013-12-04 12:03:19 -0800

    modded readme

--- install-from-same-dir-as-me.sh
+++ install-from-same-dir-as-me.sh
@@ -2,7 +2,7 @@
 wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
 dpkg -i puppetlabs-release-precise.deb
 apt-get update
-apt-get install puppet
+apt-get install puppet -y
 #
 
 cp puppet/manifests/* /etc/puppet/manifests

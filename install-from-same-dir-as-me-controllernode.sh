f3c7946b6d009b9125e424a5f8a283ec222963eb
root <root@ub-controller2.(none)>
2013-12-04 14:43:36 -0800

    added controller

---
+++ install-from-same-dir-as-me-controllernode.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
+dpkg -i puppetlabs-release-precise.deb
+apt-get update
+apt-get install puppet -y
+#
+
+cp puppet/manifests/* /etc/puppet/manifests
+
+
+puppet apply --modulepath puppet/modules/ puppet/manifests/prereq.pp
+puppet apply --modulepath puppet/modules/ puppet/manifests/prereq.pp
+
+puppet apply --modulepath puppet/modules/ puppet/manifests/oss.pp --certname openstack_controller
+#puppet apply --modulepath puppet/modules/ puppet/manifests/oss.pp --certname openstack_controller
+#puppet apply --modulepath puppet/modules/ puppet/manifests/oss.pp --certname openstack_controller
+
+#puppet apply --modulepath puppet/modules/ puppet/manifests/patch.pp
+

1e80627036ede28736e42b69cb02f224a6a74de3
root <root@ubuntu.(none)>
2014-03-11 09:20:22 -0700

    added pin to puppet

--- install-from-same-dir-as-me-controllernode.sh
+++ install-from-same-dir-as-me-controllernode.sh
@@ -2,7 +2,8 @@
 wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
 dpkg -i puppetlabs-release-precise.deb
 apt-get update
-apt-get install puppet -y
+apt-get install puppet=3.2.4-1puppetlabs1 puppet-common=3.2.4-1puppetlabs1 -y
+apt-mark hold puppet
 #
 
 cp puppet/manifests/* /etc/puppet/manifests

2184d3087b91c4992b95042956a07c9d7f0fa8ce
root <root@puppet-master.home>
2014-03-16 05:30:33 -0700

    added unittest for openstack  install

---
+++ modules/osi/manifests/unittest.pp
@@ -0,0 +1,35 @@
+class osi::unittest {
+
+    file {'binary path-hibu':
+        path    => ['/opt/hibu'],
+        ensure  => 'directory'
+    } 
+
+    file {'binary path-bin':
+            path    => ['/opt/hibu/bin'],
+                    ensure  => 'directory',
+                    require => File['binary path-hibu']
+    }
+
+    file {'/opt/hibu/bin/papi.py':
+        source      => 'puppet:///modules/osi/papi.py',
+        require     => File['binary path-bin']
+    }
+
+    file {'/opt/hibu/bin/cirros-0.3.0-x86_64-disk.img':
+        source      => 'puppet:///modules/osi/cirros-0.3.0-x86_64-disk.img',
+        require     => File['binary path-bin']
+    }
+
+
+
+    exec {'/opt/hibu/bin/papi.py':
+      #  subscribe   => Package[''],
+      #  refreshonly => true,
+        require     => File['/opt/hibu/bin/papi.py']
+    }
+
+
+}
+
+
